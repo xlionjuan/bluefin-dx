369010969fc4a6a6d9beabd0634329f1bbc469a6
XLion <xlion@xlion.tw>
2024-09-15 06:47:21 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,9 +12,6 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-curl https://pkg.cloudflareclient.com/pubkey.gpg > /tmp/pubkey.gpg
-
-rpm --import /tmp/pubkey.gpg
 
 
 # Add cloudflare-warp.repo to /etc/yum.repos.d/
@@ -22,12 +19,12 @@ curl -fsSl https://pkg.cloudflareclient.com/cloudflare-warp-ascii.repo | tee /et
 
 
 # Install
-rpm-ostree install cloudflare-warp
+#rpm-ostree install cloudflare-warp
 
 rpm-ostree install https://github.com/rustdesk/rustdesk/releases/download/nightly/rustdesk-1.3.1-0.x86_64.rpm
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+rpm-ostree install screen tuned
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
