880fe014760faabb1946529d07ae72f06bda463a
XLion <xlion@xlion.tw>
2024-09-15 06:33:03 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,28 +12,9 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+curl https://pkg.cloudflareclient.com/pubkey.gpg > /tmp/pubkey.gpg
 
-
-# Create symlinks to fix packages that create directories in /opt
-get_yaml_array OPTFIX '.optfix[]' "$1"
-if [[ ${#OPTFIX[@]} -gt 0 ]]; then
-    echo "Creating symlinks to fix packages that install to /opt"
-
-# Create symlink for /opt to /var/opt since it is not created in the image yet
-mkdir -p "/var/opt"
-ln -s "/var/opt"  "/opt"
-# Create symlinks for each directory specified in recipe.yml
-for OPTPKG in "${OPTFIX[@]}"; do
-    OPTPKG="${OPTPKG%\"}"
-    OPTPKG="${OPTPKG#\"}"
-    OPTPKG=$(printf "$OPTPKG")
-    mkdir -p "/usr/lib/opt/${OPTPKG}"
-    ln -s "../../usr/lib/opt/${OPTPKG}" "/var/opt/${OPTPKG}"
-    echo "Created symlinks for ${OPTPKG}"
-done
-fi
-
-
+rpm --import /tmp/pubkey.gpg
 
 
 # Add cloudflare-warp.repo to /etc/yum.repos.d/
