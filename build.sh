0ec87bf33fa8d245703ed97717da486225ae2144
XLion <xlion@xlion.tw>
2024-09-24 16:34:51 +0800

    fix script

--- build.sh
+++ build.sh
@@ -20,9 +20,9 @@ mkdir -p "/var/opt"
 ln -s "/var/opt"  "/opt"
 
 # Scripts
-bash scripts/rustdesk.sh
-bash scripts/zerotier.sh
-#bash scripts/intel-one-api.sh
+bash /home/runner/work/bluefin-dx/bluefin-dx/scripts/rustdesk.sh
+bash /home/runner/work/bluefin-dx/bluefin-dx/scripts/zerotier.sh
+#bash /home/runner/work/bluefin-dx/bluefin-dx/scripts/intel-one-api.sh
 
 
 # Add cloudflare-warp.repo to /etc/yum.repos.d/
