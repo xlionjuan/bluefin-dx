1b44af244688a0eaec7c29542326fdc29eb462e1
XLion <xlion@xlion.tw>
2024-10-09 21:35:46 +0800

    stable-20241001

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ curl -fsSl https://pkg.cloudflareclient.com/cloudflare-warp-ascii.repo | tee /et
 curl -fsSl https://xlionjuan.github.io/rustdesk-rpm-repo/nightly.repo | sudo tee /etc/yum.repos.d/xlion-rustdesk-rpm-repo.repo
 
 # Install
-rpm-ostree install cloudflare-warp screen tuned waydroid ntpd-rs rustdesk bzip2 cabextract libwebp-tools
+rpm-ostree install cloudflare-warp screen tuned waydroid ntpd-rs rustdesk libwebp-tools
 
 #rpm-ostree install https://github.com/Open-Wine-Components/umu-launcher/releases/download/1.1.1/umu-launcher-1.1.1-1.20241004.12ebba1.fc40.noarch.rpm
 
