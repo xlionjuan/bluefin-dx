408ef4cb8b21c05a81e7e75fe16c428b4c20e18c
XLion <xlion@xlion.tw>
2024-10-09 17:58:39 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -26,9 +26,9 @@ curl -fsSl https://pkg.cloudflareclient.com/cloudflare-warp-ascii.repo | tee /et
 curl -fsSl https://xlionjuan.github.io/rustdesk-rpm-repo/nightly.repo | sudo tee /etc/yum.repos.d/xlion-rustdesk-rpm-repo.repo
 
 # Install
-rpm-ostree install cloudflare-warp screen libwebp-tools tuned waydroid ntpd-rs rustdesk
+rpm-ostree install cloudflare-warp screen tuned waydroid ntpd-rs rustdesk bzip2-devel libwebp-tools
 
-rpm-ostree install https://github.com/Open-Wine-Components/umu-launcher/releases/download/1.1.1/umu-launcher-1.1.1-1.20241004.12ebba1.fc40.noarch.rpm
+#rpm-ostree install https://github.com/Open-Wine-Components/umu-launcher/releases/download/1.1.1/umu-launcher-1.1.1-1.20241004.12ebba1.fc40.noarch.rpm
 
 #### Example for enabling a System Unit File
 
