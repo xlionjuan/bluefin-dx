cbfdde594b0274ee4d2107f5e604e97f4e74790f
XLion <xlion@xlion.tw>
2024-11-14 22:09:14 +0800

    SELinux is terrible

--- build.sh
+++ build.sh
@@ -37,7 +37,7 @@ curl -fsSl https://pkg.cloudflareclient.com/cloudflare-warp-ascii.repo | tee /et
 curl -fsSl https://xlionjuan.github.io/rustdesk-rpm-repo/nightly.repo | tee /etc/yum.repos.d/xlion-rustdesk-rpm-repo.repo
 
 # Install
-rpm-ostree install cloudflare-warp screen tuned waydroid ntpd-rs sudo-rs rustdesk_selinux libwebp-tools wireshark
+rpm-ostree install cloudflare-warp screen tuned waydroid ntpd-rs sudo-rs rustdesk libwebp-tools wireshark
 
 #rpm-ostree install https://github.com/Open-Wine-Components/umu-launcher/releases/download/1.1.1/umu-launcher-1.1.1-1.20241004.12ebba1.fc40.noarch.rpm
 
