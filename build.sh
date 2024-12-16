0ac7fa5f0c27cdd0495527312b4dff25de8604fe
XLion <xlion@xlion.tw>
2024-12-16 23:41:53 +0800

    test

--- build.sh
+++ build.sh
@@ -33,7 +33,7 @@ curl -fsSl https://xlionjuan.github.io/rustdesk-rpm-repo/nightly.repo | tee /etc
 dnf5 upgrade -y dnf5
 
 # Install
-dnf5 install -y cloudflare-warp zerotier-one rustdesk screen tuned waydroid ntpd-rs sudo-rs libwebp-tools wireshark koji rclone
+dnf5 install -y cloudflare-warp zerotier-one screen tuned waydroid ntpd-rs sudo-rs libwebp-tools wireshark koji rclone
 
 #dnf5 install -y https://github.com/21pages/rustdesk/releases/download/revert_linux_use_cpal_build/rustdesk-1.3.5-0.x86_64.rpm
 
@@ -52,7 +52,7 @@ dnf5 install -y cloudflare-warp zerotier-one rustdesk screen tuned waydroid ntpd
 #### Example for enabling a System Unit File
 
 systemctl enable warp-svc.service
-systemctl enable rustdesk.service
+#systemctl enable rustdesk.service
 systemctl enable zerotier-one
 
 ## Use ntpd-rs to replace chronyd
