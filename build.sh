984bcd2a636b46fa0ae2c29e7b70189732774818
XLion <xlion@xlion.tw>
2024-12-20 16:10:51 +0800

    Add usrlocal symlink

--- build.sh
+++ build.sh
@@ -14,10 +14,10 @@ RELEASE="$(rpm -E %fedora)"
 
 
 # https://github.com/blue-build/modules/blob/bc0cfd7381680dc8d4c60f551980c517abd7b71f/modules/rpm-ostree/rpm-ostree.sh#L16
-echo "Creating symlinks to fix packages that install to /opt"
+echo "Creating multiple symlinks that didn't created in the image yet"
 # Create symlink for /opt to /var/opt since it is not created in the image yet
-mkdir -p "/var/opt"
-ln -s "/var/opt"  "/opt"
+mkdir -p "/var/opt" && ln -s "/var/opt"  "/opt"
+mkdir -p "/var/usrlocal" && ln -s "/var/usrlocal" "/usr/local"
 
 
 # Remove tuned-ppd to prevent GNOME touching tuned
