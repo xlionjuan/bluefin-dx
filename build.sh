85b5b13e07fbf5b7fc98c835256502f5ecc33e54
XLion <xlion@xlion.tw>
2025-01-08 22:06:40 +0800

    Metapackage for Cachyos kernel

--- build.sh
+++ build.sh
@@ -65,7 +65,7 @@ systemctl enable ntpd-rs
 dnf5 -y remove kernel kernel-headers kernel-core kernel-modules kernel-modules-core kernel-modules-extra zram-generator-defaults
 dnf5 copr enable -y bieszczaders/kernel-cachyos-lto
 dnf5 copr enable -y bieszczaders/kernel-cachyos-addons
-rpm-ostree install kernel-cachyos-lto kernel-cachyos-lto-headers
+rpm-ostree install kernel-cachyos-lto kernel-cachyos-lto-devel-matched
 dnf5 -y install scx-scheds cachyos-settings uksmd
 systemctl enable scx.service
 systemctl enable uksmd.service
