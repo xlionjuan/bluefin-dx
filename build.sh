b46cde79d6a949fe9411a7c20184d8ade2f10513
XLion <xlion@xlion.tw>
2025-02-17 11:46:46 +0800

    Disable CachyOS kernel

--- build.sh
+++ build.sh
@@ -62,10 +62,10 @@ systemctl mask chronyd
 systemctl enable ntpd-rs
 
 # CachyOS Kernel
-dnf5 -y remove kernel kernel-headers kernel-core kernel-modules kernel-modules-core kernel-modules-extra zram-generator-defaults
-dnf5 copr enable -y bieszczaders/kernel-cachyos-lto
-dnf5 copr enable -y bieszczaders/kernel-cachyos-addons
-rpm-ostree install kernel-cachyos-lts-lto kernel-cachyos-lts-lto-devel-matched
-dnf5 -y install scx-scheds cachyos-settings uksmd
-systemctl enable scx.service
+# dnf5 -y remove kernel kernel-headers kernel-core kernel-modules kernel-modules-core kernel-modules-extra zram-generator-defaults
+# dnf5 copr enable -y bieszczaders/kernel-cachyos-lto
+# dnf5 copr enable -y bieszczaders/kernel-cachyos-addons
+# rpm-ostree install kernel-cachyos-lts-lto kernel-cachyos-lts-lto-devel-matched
+# dnf5 -y install scx-scheds cachyos-settings uksmd
+# systemctl enable scx.service
 #systemctl enable uksmd.service # I don't know why
